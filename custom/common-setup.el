2c036e4dfbb3e885e5c5e838b2fef8c2ae9452b8
Marc Jeanson <git@marcjeanson.com>
2014-12-16 10:34:06 +0100

    prefer utf-8

--- custom/common-setup.el
+++ custom/common-setup.el
@@ -1,6 +1,9 @@
 ;; Turn off the menu bar
 (menu-bar-mode -1)
 
+;; Prefer utf-8 encoding
+(prefer-coding-system 'utf-8)
+
 ;; make yes or no answers for emacs y or n
 (defalias 'yes-or-no-p 'y-or-n-p)
 
