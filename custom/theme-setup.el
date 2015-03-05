e4ec7a80fd9c61cc19335f10d7d69f0bb7c05cdd
Marc Jeanson <git@marcjeanson.com>
2015-03-05 22:23:07 +0100

    trying zenburn

--- custom/theme-setup.el
+++ custom/theme-setup.el
@@ -1,3 +1,4 @@
-(set-default-font "Inconsolata 14")
-(load-theme 'afternoon t)
+(set-frame-font "Inconsolata 14")
+;; (load-theme 'afternoon t)
+(load-theme 'zenburn t)
 (set-face-background 'hl-line "#3e4446")
